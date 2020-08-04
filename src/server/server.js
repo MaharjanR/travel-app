01592cf1c84f8cb75642879b652edb00a7da450c
Ritesh <catalight.github.io>
2020-08-04 18:36:23 -0400

    finished adding API and made skeleton for now not to use too many calls

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const axios = require("axios");
 
 const app = express();
+dotenv.config();
 
-let projectData = {};
+let projectData = {
+    minTemp: "",
+    maxTemp: "",
+    temp: "",
+    minFeelsLike: "",
+    maxFeelsLike: "",
+    countryCode: "",
+    cityName: "",
+    timezone: "",
+    img: "",
+};
 
 /* Middleware*/
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,31 +26,71 @@ app.use(cors());
 
 app.use(express.static("client"));
 
+app.get("/", (req, res) => {
+    console.log(projectData);
+    console.log(projectData.img);
+    res.send("Hellow");
+});
+
 // post route to save data in the projectData
-app.post("/weather", async (req, res) => {
-    const {
-        name,
-        feelsLike,
-        description,
-        minTemp,
-        maxTemp,
-        temp,
-        feeling,
-    } = req.body;
+app.post("/travel", async (req, res) => {
+    projectData = {
+        minTemp: 18.9,
+        maxTemp: 25.8,
+        temp: 20.7,
+        minFeelsLike: 20.2,
+        maxFeelsLike: 22.2,
+        countryCode: "NP",
+        cityName: "Kathmandu",
+        timezone: "Asia/Kathmandu",
+        img:
+            "https://pixabay.com/photos/people-portrait-man-kathmandu-5321914/",
+    };
+    // try {
+    //     const { date, destination } = req.body;
+    //     let weatherbit;
+    //     const geoLocation = await axios.get(
+    //         `http://api.geonames.org/searchJSON?q=${destination}&maxRows=1&username=${process.env.NAME}`
+    //     );
+    //     const { lat, lng } = geoLocation.data.geonames[0];
 
-    projectData.name = name;
-    projectData.feelsLike = feelsLike;
-    projectData.description = description;
-    projectData.temp = temp;
-    projectData.minTemp = minTemp;
-    projectData.maxTemp = maxTemp;
-    projectData.feeling = feeling;
+    //     weatherbit = await axios.get(
+    //         `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lng}&key=${process.env.WEATHERBIT_API_KEY}`
+    //     );
 
+    //     const weatherData = weatherbit.data;
+
+    //     weatherData.data.forEach((val) => {
+    //         if ((val.valid_date = date)) {
+    //             projectData.minTemp = val.min_temp;
+    //             projectData.maxTemp = val.max_temp;
+    //             projectData.temp = val.temp;
+    //             projectData.minFeelsLike = val.app_min_temp;
+    //             projectData.maxFeelsLike = val.app_max_temp;
+    //         } else return;
+    //     });
+
+    //     projectData.countryCode = weatherData.country_code;
+    //     projectData.cityName = weatherData.city_name;
+    //     projectData.timezone = weatherData.timezone;
+
+    //     pixabay = await axios.get(
+    //         `https://pixabay.com/api/?key=${process.env.PIXABAY_API_KEY}&q=${destination}&image_type=photo`
+    //     );
+
+    //     const { pageURL } = pixabay.data.hits[0];
+
+    //     projectData.img = pageURL;
+    //     console.log("done");
     res.end();
+    // } catch (e) {
+    //     console.log(e);
+    // }
 });
 
 // get route, sends the projectdata object
-app.get("/weather", async (req, res) => {
+app.get("/travel", async (req, res) => {
+    console.log(projectData);
     await res.send(projectData);
 });
 
